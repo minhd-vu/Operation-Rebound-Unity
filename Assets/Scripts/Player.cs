612c47a46f8ed59825717398a4c30fc87d912efa
Minh Vu <minhd_vu@yahoo.com>
2019-12-16 22:35:10 -0500

    added health regeneration and helath prop

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,26 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    private float health;
+    private float _health;
+    public float health
+    {
+        get { return _health; }
+        set
+        {
+            _health = value;
+            if (_health > maxHealth)
+            {
+                _health = maxHealth;
+            }
+        }
+    }
+
+    [SerializeField]
+    private float maxHealth;
+    public float healthPerSecond;
+    private float regenTimer;
+    [SerializeField]
+    private float regenTime;
     [SerializeField]
     private Image healthBar;
     [SerializeField]
@@ -14,12 +33,19 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        health = 1f;
+        health = maxHealth;
+        regenTimer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if ((regenTimer += Time.deltaTime) >= regenTime)
+        {
+            health += healthPerSecond;
+            regenTimer = 0f;
+        }
+
         // Update the health bar.
         healthBar.fillAmount = health;
     }
