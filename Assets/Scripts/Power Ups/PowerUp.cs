d9e7d25d46a82f498aa7c6c09b9b1e9e029114f7
Minh Vu <minhd_vu@yahoo.com>
2019-12-18 11:44:05 -0500

    added power up particles

--- Assets/Scripts/Power Ups/PowerUp.cs
+++ Assets/Scripts/Power Ups/PowerUp.cs
@@ -7,12 +7,15 @@ public abstract class PowerUp : MonoBehaviour
 {
     [SerializeField]
     protected float duration;
+    [SerializeField]
+    protected GameObject particles;
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.CompareTag("Player"))
         {
             StartCoroutine(PickUp(collider));
+            Instantiate(particles, gameObject.GetComponent<Transform>());
         }
     }
 
