6d885994acd7d45cb03b68545971e86415c5c61b
Minh Vu <minhd_vu@yahoo.com>
2019-12-18 00:02:05 -0500

    part 1 leaderboards

---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    private void Awake()
+    {
+        entryContainer = transform.Find("Leaderboard Entry Container");
+        entryTemplate = entryContainer.Find("Leaderboard Entry Template");
+        entryTemplate.gameObject.SetActive(false);
+
+        float height = 50f;
+        for (int i = 0; i < 10; ++i)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -height * i);
+            entryTransform.gameObject.SetActive(true);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
