2dc95eb82bc53ae017a77fa250b7326ee54ccd2e
Minh Vu <minhd_vu@yahoo.com>
2019-12-18 15:58:47 -0500

    made smoother i think

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     //[SerializeField]
     //private float rotateSpeed;
     private Rigidbody2D rb;
+    private Vector2 input;
+    private Vector3 direction;
 
     // Start is called before the first frame update
     void Start()
@@ -24,18 +26,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+    }
+
+    private void FixedUpdate()
+    {
         // Move the player based on the input.
         float speed = moveSpeed + moveSpeedBonus;
 
         // Move the player slower when they are moving diagonally to maintain the same magnitude.
-        if (Math.Abs(Input.GetAxisRaw("Horizontal")) > 0 && Math.Abs(Input.GetAxisRaw("Vertical")) > 0)
+        if (Math.Abs(input.x) > 0 && Math.Abs(input.y) > 0)
         {
             speed /= Mathf.Sqrt(2);
         }
 
         //transform.Translate(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0, Space.World);
         //rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime);
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+        rb.velocity = new Vector2(input.x * speed, input.y * speed);
         //rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed));
 
         /**
@@ -44,9 +52,8 @@ public class PlayerController : MonoBehaviour
          * Find the angle and convert it into degrees.
          * Slerp the value for a smooth rotation (removed).
          **/
-        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), dir.normalized.magnitude * rotateSpeed * Time.deltaTime);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), dir.normalized.magnitude * rotationSpeed * Time.deltaTime);
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
