d12b77499dd1be5edce15e7088eebf7b0de59970
Minh Vu <minhd_vu@yahoo.com>
2019-12-19 17:05:06 -0500

    trying to add post processing

---
+++ Assets/Scripts/PostProcessing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessing : MonoBehaviour
+{
+    [SerializeField]
+    private Player player;
+    private ColorGrading colorGrading;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PostProcessVolume volume = gameObject.GetComponent<PostProcessVolume>();
+        player = gameObject.GetComponent<Player>();
+        volume.profile.TryGetSettings(out colorGrading);
+        colorGrading.enabled.value = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        colorGrading.saturation.value = Mathf.Clamp(player.health / player.maxHealth, 0, 1f);
+    }
+}
