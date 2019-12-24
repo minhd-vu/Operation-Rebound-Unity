ca5d9b2613c8c60bd79aeef52509e2a92e3d432f
Minh Vu <minhd_vu@yahoo.com>
2019-12-24 12:21:04 -0500

    added DOTween, dashing (i think)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,14 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed;
-    [HideInInspector]
-    public float moveSpeedBonus;
+    [SerializeField] private float moveSpeed;
+    [HideInInspector] public float moveSpeedBonus;
     //[SerializeField]
     //private float rotateSpeed;
     private Rigidbody2D rb;
     private Vector2 input;
     private Vector2 mousePosition;
+    private bool dodging;
 
     // Start is called before the first frame update
     void Start()
@@ -26,32 +26,34 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        // Get input from player
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Dash(duration));
+        }
     }
+
+    public IEnumerator Dash(float duration)
+    {
+        Vector3 dashVector = rb.transform.position + (Vector3)input * dashForce;
+        rb.DOMove(dashVector, duration);
+        yield return new WaitForSeconds(duration);
+    }
+
+    private float dashForce = 4f;
+    private float duration = 0.3f;
 
     private void FixedUpdate()
     {
         // Move the player based on the input.
         float speed = moveSpeed + moveSpeedBonus;
 
-        // Move the player slower when they are moving diagonally to maintain the same magnitude.
-        if (Math.Abs(input.x) > 0 && Math.Abs(input.y) > 0)
-        {
-            speed /= Mathf.Sqrt(2);
-        }
-
-        //transform.Translate(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0, Space.World);
-        //rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime);
         rb.velocity = new Vector2(input.x * speed, input.y * speed);
-        //rb.AddForce(new Vector2(input.x * speed, input.y * speed));
 
-        /**
-         * Rotate the player towards the mouse cursor.
-         * Find the direction vector.
-         * Find the angle and convert it into degrees.
-         * Slerp the value for a smooth rotation (removed).
-         **/
+        // Rotate the player
         Vector2 direction = mousePosition - rb.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), dir.normalized.magnitude * rotationSpeed * Time.deltaTime);
